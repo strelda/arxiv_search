8e54abf086cc3ea6f80b369d36130603eff3b80c
strelda <strelecekja@gmail.com>
2022-12-16 22:15:53 +0100

    bash script fix

--- arxiv_search.sh
+++ arxiv_search.sh
@@ -3,5 +3,5 @@
 search_query="abs:state_driving OR abs:qubit OR abs:quantum_computing OR abs:quantum_computer OR abs:state_manifold"
 since_date="2022-12-15"
 
-./target/debug/arxiv_search "$search_query" "$since_date" > localhost/main.html
-# && http-server localhost/
+./target/debug/arxiv_search "$search_query" "$since_date" > localhost/main.html && 
+http-server localhost/

c57d46b7609720bcb3567939920dba1e2f6bedf3
strelda <strelecekja@gmail.com>
2022-12-16 22:15:05 +0100

    bash script fix

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::env;
 
 fn main(){
     // how to construct query: https://arxiv.org/help/api/user-manual#query_details
-    // const SEARCH_QUERY: &str = "abs:state_driving OR abs:qubit OR abs:quantum_computing OR abs:quantum_computer OR abs:state_manifold";
-    // const SINCE_DATE: &str = "2022-12-15"; //"%Y-%m-%d"
+    // const search_query: &str = "abs:state_driving OR abs:qubit OR abs:quantum_computing OR abs:quantum_computer OR abs:state_manifold";
+    // const since_date: &str = "2022-12-15"; //"%Y-%m-%d"
     //input these parameters when calling the program
     let args: Vec<String> = env::args().collect();
     let search_query = &args[1];

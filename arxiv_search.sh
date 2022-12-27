a10352292e354879d2e2385aca55a88b26312255
strelda <strelecekja@gmail.com>
2022-12-27 13:11:02 +0100

    two days old

--- arxiv_search.sh
+++ arxiv_search.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 search_query="abs:state_driving OR abs:qubit OR abs:quantum_computing OR abs:quantum_computer OR abs:state_manifold"
-since_date=$(date -d "$since_date -2 days" +%Y-%m-%d)
+since_date=$(date -d "2 days ago" +%Y-%m-%d)
 
 ./release/arxiv_search "$search_query" "$since_date" > localhost/main.html && 
 http-server localhost/
